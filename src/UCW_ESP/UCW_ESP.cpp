2fbc73bd440974f224af3c7367cd0cb6acc5d2b6
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2017-12-16 23:59:35 -0800

    ok

--- src/UCW_ESP/UCW_ESP.cpp
+++ src/UCW_ESP/UCW_ESP.cpp
@@ -210,7 +210,7 @@ void UCW_ESP::updateBattStatus(){
 
 EEPROM.begin(512);
 byte battery_count = EEPROM.read(0);
- // we only need this to happen once every x minutes,
+ // we only need this to happen once every 100x runtime seconds,
   // so we use eeprom to track the count between resets.
   if(battery_count >= ((BATTERY_INTERVAL * 60) / SLEEP_LENGTH)) {
     // reset counter
@@ -232,7 +232,7 @@ void UCW_ESP::battery_level(){
 
   //read the battery level from the ESP8266 analog in pin.
   // analog read level is 10 bit 0-1023 (0V-1V).
-  // our 1M & 220K voltage divider takes the max
+  // use 1M & 220K voltage divider takes the max
   // lipo value of 4.2V and drops it to 0.758V max.
   // this means our min analog read value should be 580 (3.14V)
   // and the max analog read value should be 774 (4.2V).
--- src/UCW_M0/UCW_M0.cpp
+++ src/UCW_M0/UCW_M0.cpp
@@ -229,10 +229,10 @@ void UCW_M0::updateBattStatus(){
 float measuredvbat = analogRead(VBATPIN);
 measuredvbat *= 2;    // we divided by 2, so multiply back
 measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-measuredvbat /= 4096; // convert to voltage
+measuredvbat /= 1024; // convert to voltage
 Serial.print("VBat: " ); Serial.println(measuredvbat);
 
-Serial.println();
+
 }
 
 
