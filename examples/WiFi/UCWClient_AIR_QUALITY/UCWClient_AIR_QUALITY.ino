cb8e54b20b066e932f679e18b47196c53076a5c5
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-03-27 10:40:17 -0700

    created example for measuring air quality

---
+++ examples/WiFi/UCWClient_AIR_QUALITY/UCWClient_AIR_QUALITY.ino
@@ -0,0 +1,91 @@
+
+ /*
+  Air quality measurement using Adafruit CCS811 sensor
+  Copyright 2018 Unity{Cloud}Ware - UCW Industries Ltd. All rights reserved.
+ */
+
+#include "config.h"
+
+/*
+  Edit the config.h to configure the connection string to the UCW Platform
+  and any additional configuration needed for WiFi, cellular, or ethernet
+  clients.
+ */
+
+#include "Adafruit_CCS811.h"
+
+//comment appropriately
+   UCW_API_REST ucw_api =  ucw.api();         //REST API
+// UCW_API_MQTT ucw_api =  ucw.api_m();      //MQTT
+
+#define DEVICE_ID   "your_device_id"
+#define DATA_STREAM "Air_quality_measurements"
+
+Adafruit_CCS811 ccs; //Adafruit_CCS811 object  
+
+float temp; //ambient temperature reading
+int tvoc;  // total volatie organic compounds
+int eCO2;  // equivalent calculated carbon-dioxide 
+
+
+void setup() {
+  
+  // Start the serial connection
+  Serial.begin(9600);
+
+  while (!Serial) {
+    ; // Wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Connect to UCW IoT Cloud
+  Serial.print("Connecting to UCW IoT Cloud...");
+  ucw.connect();
+
+  // Wait for a connection
+  while (ucw.status() != UCW_NET_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // We are connected
+  Serial.println(" Connected!");
+  ucw.printNetworkInfo();
+  
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+
+  //calibrate temperature sensor
+  while(!ccs.available());
+  temp = ccs.calculateTemperature();
+  ccs.setTempOffset(temp - 25.0);
+}
+
+void loop() {
+  ucw.sys();
+
+  // read data()
+  if(ccs.available()){
+    temp = ccs.calculateTemperature();
+    if(!ccs.readData()){      
+      eCO2 = ccs.geteCO2();
+      tvoc = ccs.getTVOC();
+      }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+  String data = "{\"eCO2\": \"%eCO2\", \"TVOC\": \"%tvoc\", \"temperatureC\": \"%temperatureC\"}";
+  data.replace("%eCO2", String(eCO2));
+  data.replace("%tvoc", String(tvoc));
+  data.replace("%temperatureC", String(temp));
+  
+  ucw_api.sendData(DEVICE_ID, DATA_STREAM, data);
+  
+  delay(1000);
+ 
+}
+
+
---
+++ examples/WiFi/UCWClient_AIR_QUALITY/config.h
@@ -0,0 +1,68 @@
+
+#include <UCW_System.h>
+
+/************************ UCW Platform Config *******************************/
+
+// Configuration of the connection string to the UCW Platform.
+//for WiFi API connection only
+static UCWConfig cfg = {
+  .host = UCW_API_HOST,
+  .port = UCW_API_PORT,
+  .isSecuredConnection = false,
+  .useMqtt = false,
+  .token = "your_token"
+};
+
+//for LoRa connection only
+//static UCWConfig_Lora cfg = {
+//  .freq = RF95_FREQ,
+//  .token = "your_token"
+//};
+
+/******************************* WiFi **************************************/
+
+/*
+  The UCW_WiFi client will work with the following boards:
+    - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+    - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+    - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+    - Feather WICED -> https://www.adafruit.com/products/3056
+*/
+
+#define WIFI_SSID       "your_ssid"
+#define WIFI_PASS       "your_pass"
+
+#include "UCW_WiFi.h"
+UCW_WiFi ucw(&cfg, WIFI_SSID, WIFI_PASS);
+
+/**************************** Ethernet ************************************/
+
+/*
+  The UCW_Ethernet client will work with the following boards:
+    - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
+ */
+
+// uncomment the following two lines for ethernet,
+//#include "UCW_Ethernet.h"
+//UCW_Ethernet ucw(&cfg);
+
+/**************************** LoRa ************************************/
+
+/*
+  The UCW_LoRa client will work with the following boards:
+    - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+ */
+
+// uncomment the following two lines for LoRa,
+//#include "UCW_LoRa.h"
+//UCW_LoRa ucw(&cfg);
+
+/**************************** BLE ************************************/
+
+/*
+  The UCW_LoRa client will work with the following boards:
+    - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+ */
+
+// uncomment the following two lines for BLE,
+//#include "UCW_M0_BLE.h"
