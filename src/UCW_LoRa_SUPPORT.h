6ef6c1b2bd1aab5c2371a04ad169f4a00900587b
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-06-18 10:13:04 -0700

    deleted switch

--- src/UCW_LoRa_SUPPORT.h
+++ src/UCW_LoRa_SUPPORT.h
@@ -6,14 +6,11 @@
 #ifndef UCW_LoRaSUPPORT_H
 #define UCW_LoRaSUPPORT_H
 
-#if defined (UCW_RADIO_LORA)
-
-  #if !defined(ARDUINO_SAMD_MKR1000) && defined(ARDUINO_ARCH_SAMD)
+#if !defined(ARDUINO_SAMD_MKR1000) && defined(ARDUINO_ARCH_SAMD)
 
     #include "lora/UCW_M0LoRa.h"
     typedef UCW_M0LoRa UCW_LoRa_SUPPORT;
 
-  #endif // UCW_RADIO_LORA
 
 #endif // !ARDUINO_SAMD_MKR1000 && ARDUINO_ARCH_SAMD
 
