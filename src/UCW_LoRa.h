53267be8b91974606c187eebc0ed19fdfb5dd4d1
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-07-16 22:47:46 -0700

    corrected sendData method

--- src/UCW_LoRa.h
+++ src/UCW_LoRa.h
@@ -17,15 +17,15 @@ class UCW_LoRa {
     void connect();
     virtual ucw_status_t Operatingfreq() = 0;
     virtual String connectionType() = 0;
-    virtual bool sendData(const uint8_t* your_deviceID, const uint8_t* your_dataStreamName, String payload) = 0;
+    virtual bool sendData(String your_deviceID, String your_dataStreamName, String payload) = 0;
     void sys();
     String version();
     String userAgent();
+    virtual void receiveData() = 0;
 
   protected:
     virtual void _connect() = 0;
     virtual void _sys() = 0;
-    virtual void receiveData() = 0;
 
     //variables
     char *_version;
