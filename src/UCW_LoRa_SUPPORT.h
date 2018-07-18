23c4444b63a0c08f863432c2979b1d50ca98e59b
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-07-17 17:05:50 -0700

    added switch

--- src/UCW_LoRa_SUPPORT.h
+++ src/UCW_LoRa_SUPPORT.h
@@ -3,14 +3,15 @@
   Copyright 2018 Unity{Cloud}Ware - UCW Industries Ltd. All rights reserved.
  */
 
-#ifndef UCW_LoRaSUPPORT_H
-#define UCW_LoRaSUPPORT_H
+#ifndef UCW_LORASUPPORT_H
+#define UCW_LORASUPPORT_H
 
+#if defined(UCW_LORA_DEVICE)
 #if !defined(ARDUINO_SAMD_MKR1000) && defined(ARDUINO_ARCH_SAMD)
 
   #include "lora/UCW_M0LoRa.h"
   typedef UCW_M0LoRa UCW_LoRa_SUPPORT;
 
 #endif // !ARDUINO_SAMD_MKR1000 && ARDUINO_ARCH_SAMD
-
-#endif // UCW_LoRaSUPPORT_H
+#endif // LoRa switch
+#endif // UCW_LORASUPPORT_H
--- src/UCW_WiFi.h
+++ src/UCW_WiFi.h
@@ -6,6 +6,8 @@
 #ifndef UCW_WIFI_H
 #define UCW_WIFI_H
 
+#if defined(UCW_WIFI_DEVICE)
+
 #if !defined(ARDUINO_SAMD_MKR1000) && defined(ARDUINO_ARCH_SAMD)
 
   #include "wifi/UCW_WINC1500.h"
@@ -22,4 +24,5 @@
   typedef UCW_ESP8266 UCW_WiFi;
 
 #endif //board switch
+#endif // WiFi switch
 #endif // UCW_WIFI_H
