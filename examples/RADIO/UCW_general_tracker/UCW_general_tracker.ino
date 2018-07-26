603311905e9c859e50e6ccba27e5073a13af5113
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-07-26 08:22:13 -0700

    corrected tx method

--- examples/RADIO/UCW_general_tracker/UCW_general_tracker.ino
+++ examples/RADIO/UCW_general_tracker/UCW_general_tracker.ino
@@ -17,23 +17,16 @@
 #include <UCW_GPS.h>
 #include <UCW_LoRa_WAN.h>
 
-uint8_t DATA_STREAM[] = "ucw-general-tracker";
+#define DATA_STREAM "ucw-general-tracker"
 
 //create GPS object
 UCW_GPS ucw_gps; 
 
-//create UCW_LoRa_WAN object
-UCW_LoRa_WAN lora_wan;
-
 //declaring variables
 double Latitude, Longitude;
 float battLevel;
-
-//Json string
-String data;
-
-// Track if the current message has finished sending
-bool dataSent = false;
+static osjob_t sendjob;
+String data;  //Json string
 
 // Schedule TX every this many seconds (might become longer due to duty cycle limitations).
 const unsigned TX_INTERVAL = 5;
@@ -56,69 +49,55 @@ void setup() {
   //LoRaWAN steup
   lora_wan.loraWanSetup();
   lora_wan.setConfig(false);
+
+  //Start job
+  do_send(&sendjob);
 }
 
 void loop() {
-  readData();
+  os_runloop_once();
 }
 
-void readData(){
-  // Ensure there is not a current TX/RX job running
-  if (LMIC.opmode & (1 << 7)) {
-    // Something already in the queque
-    return;
-  }
-
+void do_send(osjob_t* j){
   // read GPS info
   gpsParams gpsData = ucw_gps.readGPS(); 
   Latitude = gpsData.Latitude;
   Longitude = gpsData.Longitude;
 
-  //read battery level
-  battLevel = ucw.updateBatteryStatus();
-
-  data = "{\"latitude\": %lat,\"longitude\": %long,\"Battery\": \"%bat\"}";
+  data = "{\"latitude\": %lat,\"longitude\": %long,\"DataStreamName\": \"%dsn\"}";
   data.replace("%lat", String(Latitude));
   data.replace("%long", String(Longitude));
   data.replace("%bat", String(battLevel));
+  data.replace("%dsn", DATA_STREAM);
 
   //convert string to char
   int len = data.length()+1; // length of payload data
   char mydata[len];
   data.toCharArray(mydata, sizeof(mydata));
 
-  //Prepare upstream data transmission at the next possible time.
-  LMIC_setTxData2(1, (uint8_t*)mydata, sizeof(mydata), 0);
-  Serial.println(F("Packet queued"));
-
-  // Wait for the data to send or timeout after 15s
-  int sinceSend = 0;
-  while (!dataSent && sinceSend < 15000) {
-    os_runloop_once();
-    sinceSend++;
-    delay(1);
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    //Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    Serial.println(F("Packet queued"));
   }
 }
 
 void onEvent (ev_t ev) {
   if (ev == EV_TXCOMPLETE) {
-    dataSent = true;
     Serial.println(F("transmission completed"));
-    //check if data received from server
+    if (LMIC.txrxFlags & TXRX_ACK) Serial.println(F("Received ack"));
     if (LMIC.dataLen) {
-      // data received in rx slot after tx
-      Serial.print(F("Received "));
-      Serial.print(LMIC.dataLen);
-      Serial.print(F(" bytes of payload: 0x"));
-      for (int i = 0; i < LMIC.dataLen; i++) {
-        if (LMIC.frame[LMIC.dataBeg + i] < 0x10) {
-          Serial.print(F("0"));
-          }
-        //print received data
-        Serial.println(LMIC.frame[LMIC.dataBeg + i], HEX);
-      }
+      Serial.println(F("Received "));
+      Serial.println(LMIC.dataLen);
+      Serial.println(F(" bytes of payload"));
     }
+    // Schedule next transmission
+    os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
   }
   if (ev == EV_LINK_DEAD) {
     lora_wan.loraWanSetup();
-    }
+  }
+}
