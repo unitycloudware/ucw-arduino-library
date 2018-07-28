95e48b170c070f92a213802c61b15900f288bead
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-07-28 11:39:23 -0700

    added config files for LORA and BLE

---
+++ src/UCW_LoRaWAN_Config.h
@@ -0,0 +1,21 @@
+
+/*
+  Arduino library to access UCW Platform
+   This file will contain config info for LoRa library
+  Copyright 2018 Unity{Cloud}Ware - UCW Industries Ltd. All rights reserved.
+ */
+
+#ifndef UCW_LORAWAN_CONFIG
+#define UCW_LORAWAN_CONFIG
+
+
+//Switches for LORA AND BLE
+#define UCW_LORA_DEVICE    //LORA switch
+
+//variables for LoRaWAN. Comment appropriately
+#define UCW_LORA_ABP
+//#define UCW_LORA_OTTA
+
+#endif //UCW_CONFIG_H
+
+
