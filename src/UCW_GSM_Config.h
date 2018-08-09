20eaed4d931c778cdc34d730e132f501e4e794d7
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-08-08 22:47:21 -0700

    config file for GSM lib

---
+++ src/UCW_GSM_Config.h
@@ -0,0 +1,18 @@
+
+/*
+  Arduino library to access UCW Platform
+  This file will contain config info for GSM library
+  Copyright 2018 Unity{Cloud}Ware - UCW Industries Ltd. All rights reserved.
+ */
+
+#ifndef UCW_GSM_CONFIG
+#define UCW_GSM_CONFIG
+
+//Switches for GSM
+#define UCW_GSM_DEVICE    //GSM switch
+
+#endif //UCW_GSM_CONFIG
+
+
+
+
