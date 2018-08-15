07d15d30a5a0e6a6988e5e1afac11acdcd9a48f3
Nnamdi Eyisi <nnamdieyisi@yahoo.com>
2018-08-15 15:11:17 -0700

    created config file for switch

---
+++ src/UCW_Config.h
@@ -0,0 +1,20 @@
+
+/*
+  Arduino library to access UCW Platform
+  This file contains switch info for the library
+  Copyright 2018 Unity{Cloud}Ware - UCW Industries Ltd. All rights reserved.
+ */
+
+#ifndef UCW_CONFIG
+#define UCW_CONFIG
+
+//switches
+//#define UCW_BLE_DEVICE    //BLE switch
+//#define UCW_ETHERNET_DEVICE    //Ethernet switch
+//#define UCW_GSM_DEVICE    //GSM switch
+//#define UCW_LORA_DEVICE    //LORA switch
+#define UCW_WIFI_DEVICE     //WiFi switch
+
+#endif //UCW_CONFIG_H
+
+
